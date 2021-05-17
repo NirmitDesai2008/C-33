79cb26fe4b1c3373fdecb06818610becea63fefc
NirmitDesai2008 <80904497+NirmitDesai2008@users.noreply.github.com>
2021-05-17 19:35:43 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,21 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.remove = true;
+  }
+
+display(){
+  if((this.body.speed)<2.5){
+    super.display();
+  }
+  else{
+    World.remove(world,this.body);
+
+    if (this.remove === true){
+      score = score+100;
+      this.remove = false;
+    }
+  }
+}
+}
